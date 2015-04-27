82d7ed89b49cd3043171a18774c3734e61a7268d
Hans Hörberg <hans.horberg@umu.se>
2015-04-27 16:40:43 +0200

    Added build and run scripts for windows.

---
+++ dockerfiles/build.ps1
@@ -0,0 +1,23 @@
+Write-Host "Start build"
+$repository = "itsdirg/pefim_sp"
+
+$ssh_path = "c:\Program Files (x86)\Git\bin"
+IF (!($Env:Path | Select-String -SimpleMatch $ssh_path)){
+	$Env:Path = "${Env:Path};$ssh_path"
+}
+
+IF ($Env:DOCKER_HOST.length -eq 0){
+	boot2docker shellinit | Invoke-Expression
+}
+
+#Remove windows CR 
+
+$files = gci ../ *.* -File -rec | where {! $_.ps1} | %{$_.FullName}
+foreach ($file in $files){
+	sed -i 's/\r//' ${file}
+}
+
+docker rmi -f "${repository}"
+docker build -t="${repository}" .
+
+Write-Host "End build"
